0786e9e58215e9d8605a15d665188ce9ef5610a9
Olzhas Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2023-07-10 21:08:24 +0600

    docker some correct chg

--- adminpanel/src/main/java/kz/management/adminpanel/config/ApplicationConfig.java
+++ adminpanel/src/main/java/kz/management/adminpanel/config/ApplicationConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class ApplicationConfig {
 
+
     private final UserDetailsService userDetailsService;
 
     public ApplicationConfig(UserDetailsService userDetailsService) {
